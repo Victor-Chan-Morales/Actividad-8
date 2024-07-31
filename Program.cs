a0a325523eee01035d609b63c1419231cec36da1
Víctor_3.1416 <matemorales2020@gmail.com>
2024-07-31 10:08:52 -0600

    Mod

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
 ﻿using Actividad_8;
 
 List<Receta> listaRecetas = new List<Receta>();
+Receta NuevaLista = new Receta("", 0);
 bool ingresar = false;
 do
 {
     Console.Clear();
-    Console.WriteLine("");
+    Console.WriteLine("Menú principal\n1. Registrar una receta\n2. Buscar una receta\n. 3. Ver recetas disponibles\n4. Salir del programa\nSeleccione una opción:");
+    int opciónMenú=int.Parse(Console.ReadLine());
+    switch(opciónMenú)
+    {
+        case 1:
+            NuevaLista.PedirDatos();
+            listaRecetas.Add(NuevaLista);
+            break;
+        case 2:
+            NuevaLista.BuscarReceta(listaRecetas);
+            break;
+        case 3:
+            NuevaLista.MostrarRecetas(listaRecetas);
+            break;
+        case 4:
+            ingresar = false;
+            break;
+    }
+
 } while (ingresar);
 
